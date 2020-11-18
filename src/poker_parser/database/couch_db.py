210b4fad8d11f4e0a2bf791c1b85a8d4627dfe53
Chris060986 <christoph.birk@gmail.com>
2020-11-18 20:42:00 +0100

    Use pokerhand id as database id and overwrite in conflict cases

--- src/poker_parser/database/couch_db.py
+++ src/poker_parser/database/couch_db.py
@@ -18,5 +18,28 @@ class CouchDBAccess:
             return self.couch_db_server.create(hero)
 
     def save_poker_hand(self, hero, hand_history_doc):
+        """Creates and updates the poker hand. If the hand is not available in the database it will be created.
+           If available it will be overwritten. NO MERGE!
+
+
+            Parameters
+            ----------
+            hero : str
+                The name of the pokerhero used as db name
+            hand_history_doc : doc
+                The json document of the poker hand
+
+            Returns
+            -------
+            (id, rev)
+                tuple of id and rev of the saved document
+            """
+        _id = str(hand_history_doc['id'])
         db = self.get_db(hero)
-        db.save(hand_history_doc)
+        hand_history_doc['_id'] = _id
+        row = db.get(_id)
+        print(_id)
+        if row is not None:
+            print(row['_rev'])
+            hand_history_doc['_rev'] = row['_rev']
+        return db.save(hand_history_doc)
