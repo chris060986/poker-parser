6fd28d2ca1a49019f6260adaf5967e42aa2c4017
Chris060986 <christoph.birk@gmail.com>
2020-12-21 13:36:05 +0100

    update version after release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name="pokerstars_parser",
-    version="0.0.1",
+    version="0.0.2",
     description="Pokerstars HandHistory Parser",
     classifiers=classifiers,
     keywords="poker",
